b95a6957600ee24355ea5efb91dba12259dc6a63
blanca <978059629@qq.com>
2022-04-27 16:08:14 +0800

    wide Screen

--- like bilibili button/index.js
+++ like bilibili button/index.js
@@ -1,24 +1,28 @@
 // 播放
 let play_state = true;
-const playWrapper = document.getElementById('playWrapper')
+const playWrapper = document.getElementById('playWrapper');
 const play_open = document.getElementsByClassName('play_open')[0];
 const play_close = document.getElementsByClassName('play_close')[0];
-playWrapper.addEventListener('click', function(){
-  console.log('123')
-  if(play_state){
-    play_open.classList = 'play_open play_show'
-    play_close.classList = 'play_close'
-  }else{
-    play_open.classList = 'play_open'
-    play_close.classList = 'play_close play_show'
+playWrapper.addEventListener('click', function () {
+  console.log('123');
+  if (play_state) {
+    play_open.classList = 'play_open play_show';
+    play_close.classList = 'play_close';
+  } else {
+    play_open.classList = 'play_open';
+    play_close.classList = 'play_close play_show';
   }
-  play_state = !play_state
-})
+  play_state = !play_state;
+});
 //字幕
 let subtitle_state = true;
 const subtitleContent = document.getElementById('subtitleContent');
-const subtitle_fontAtOpen = document.getElementsByClassName('subtitle_fontAtOpen')[0];
-const subtitle_fontAtClose = document.getElementsByClassName('subtitle_fontAtClose')[0];
+const subtitle_fontAtOpen = document.getElementsByClassName(
+  'subtitle_fontAtOpen'
+)[0];
+const subtitle_fontAtClose = document.getElementsByClassName(
+  'subtitle_fontAtClose'
+)[0];
 const subtitle_line = document.getElementsByClassName('subtitle_line')[0];
 subtitleContent.addEventListener('click', function () {
   if (subtitle_state) {
@@ -33,4 +37,17 @@ subtitleContent.addEventListener('click', function () {
     subtitle_fontAtClose.style.display = 'none';
   }
   subtitle_state = !subtitle_state;
+});
+// 音量
+let volume_state = true;
+const volumeWrapper = document.getElementById('volumeWrapper');
+const volume_content = document.getElementsByClassName('volume_content')[0];
+
+volumeWrapper.addEventListener('click', function () {
+  if (volume_state) {
+    volume_content.classList = 'volume_content volume_close';
+  } else {
+    volume_content.classList = 'volume_content volume_open';
+  }
+  volume_state = !volume_state;
 });
